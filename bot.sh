677313bce6d749921bb56fc76d3505f28fa5c52e
Phoethar12 <89362791+phoethar1@users.noreply.github.com>
2021-11-30 09:54:43 +0630

    Add files via upload

---
+++ bot.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+pip install lolcat
+figlet hello! | lolcat
+clear
+
+figlet hello! | lolcat
+
+
+figlet "maayeloe minaunghlaing" |lolcat
+clear
+
+figlet ma | lolcat
+figlet aye | lolcat
+figlet loe | lolcat
+
+figlet min | lolcat
+figlet aung | lolcat 
+figlet hlaing | lolcat 
+
+echo maayeloeminaunghlaing!! |lolcat
+clear
+
+echo 🍌🍌🍌🍌| lolcat
+clear
